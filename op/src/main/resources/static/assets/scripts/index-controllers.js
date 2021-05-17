a721f3fafb8dfcc2a4c3b9f5183570c0de9d4d0b
walter_zero <249506369@qq.com>
2021-05-17 18:55:44 +0800

    添加展示列表model-2
    添加展示列表model-2

--- op/src/main/resources/static/assets/scripts/index-controllers.js
+++ op/src/main/resources/static/assets/scripts/index-controllers.js
@@ -1,9 +1,32 @@
 /***********************************************
  * 页面初始化，find用户登录状态
  ***********************************************/
+function setModelDiv (model) {
+    let html = ''
+    for (var i = 0; i < model.length; i++) {
+        var it = model[i];
+        html += '<div class="vlt-timeline-item">' +
+            '<div class="row">' +
+                '<label class="col-xl-2">' +
+                    '<input style="margin-top: 0.4rem;" type="radio" name="tempList" value="' + it.name + '">' +
+                '</label>' +
+               '<div class="col-xl-4">' +
+                '<span class="vlt-timeline-item__date">2021-01-12 12:21:33</span>' +
+                    '<h5 class="vlt-timeline-item__title">' + it.name + '</h5>' +
+                    '</div>' +
+                '<div class="col-xl-6">' +
+                    '<p class="vlt-timeline-item__text">' + it.remark + '</p>' +
+                '</div>' +
+            '</div>' +
+        '</div>'
+    }
+    $('#templist').html(html)
+}
 var formData = new FormData()
 formData.append('property', $.cookie('ygg1412'))
 $('.loading-div').show()
+var model = []
+
 $.ajax({
     type: 'POST',
     url: '/initUserInfo',
@@ -18,6 +41,9 @@ $.ajax({
             if(window.location.pathname.indexOf("login.html")!=-1){//登录成功如果当前是login也跳转index
                 location.href = "index.html"
             }
+            console.log(data.model)
+            model = data.model || []
+            setModelDiv(model)
         } else {
             $.removeCookie('ygg1412', {domain: location.hostname});
             location.href = "login.html#User"
@@ -31,6 +57,14 @@ $.ajax({
         alert('请求失败')
     }
 });
+$('#gotoengine').click(function() {
+    model = $("input[name='tempList']:checked").val()
+    if (model) {
+        window.open('engine.html?userc=' + $.cookie('ygg1412')+'&model=' + model, "_blank")
+    } else {
+        alert('请选择一条数据')
+    }
+})
 /***********************************************
  * 创建模版
  ***********************************************/
